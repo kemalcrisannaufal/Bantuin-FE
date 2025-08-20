2dcff462322c92981f3a478382cc583a4dc93d76
kemalcrisannaufal <kemalcrisannnaufal@gmail.com>
2025-08-20 13:33:53 +0700

    style: change imae to text on auth layout

--- src/components/layouts/AuthLayout/AuthLayout.tsx
+++ src/components/layouts/AuthLayout/AuthLayout.tsx
@@ -1,5 +1,5 @@
 import PageHead from "@/components/common/PageHead";
-import Image from "next/image";
+// import Image from "next/image";
 import { ReactNode } from "react";
 
 interface Proptypes {
@@ -10,17 +10,24 @@ interface Proptypes {
 const AuthLayout = (props: Proptypes) => {
   const { title, children } = props;
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen">
+    <div className="flex flex-col justify-center items-center md:px-10 min-h-screen">
       <PageHead title={title} />
-      <div className="flex md:flex-row flex-col justify-center items-center gap-5 w-full">
-        <div className="relative w-[250px] md:w-[350px] h-[250px] md:h-[350px]">
-          <Image
+      <div className="flex md:flex-row flex-col justify-center items-center xl:gap-5 w-full">
+        <div className="flex flex-col justify-center items-start w-[250px] md:w-[350px] h-[250px] md:h-[350px]">
+          <span className="font-bold text-blue-700 text-3xl md:text-5xl">
+            Bantuin
+          </span>
+          <p className="font-medium text-neutral-700 md:text-lg">
+            Atur Harimu, Gampang Bareng Bantuin
+          </p>
+
+          {/* <Image
             src={"/images/general/logo.png"}
             alt="logo"
             width={350}
             height={350}
             className="w-[250px] md:w-[350px] h-[250px] md:h-[350px] object-cover"
-          />
+          /> */}
         </div>
 
         <div className="p-5 w-full md:max-w-md">{children}</div>
